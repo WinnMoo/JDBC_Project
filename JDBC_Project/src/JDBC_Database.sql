fa5b041b002c2d4573e5b086ee9f3eb3eccef290
WinnMoo <winnmoo@gmail.com>
2017-10-22 20:17:16 -0700

    .

--- JDBC_Project/src/JDBC_Database.sql
+++ JDBC_Project/src/JDBC_Database.sql
@@ -7,9 +7,9 @@ CONSTRAINT WritingGroup_PK PRIMARY KEY (GroupName)
 );
 
 CREATE TABLE Publisher (
-PublisherName varchar(20) NOT NULL,
+PublisherName varchar(30) NOT NULL,
 PublisherAddress varchar(50) NOT NULL,
-PublisherPhone varchar(10) NOT NULL,
+PublisherPhone varchar(15) NOT NULL,
 PublisherEmail varchar(30) NOT NULL,
 CONSTRAINT Publisher_PK PRIMARY KEY(PublisherName)
 );
@@ -18,25 +18,23 @@ CREATE TABLE Book (
 BookTitle varchar(50) NOT NULL,
 YearPublished varchar(5) NOT NULL,
 NumberPages varchar(10) NOT NULL,
-PublisherName varchar(20) NOT NULL,
+PublisherName varchar(30) NOT NULL,
 GroupName varchar(50) NOT NULL,
 CONSTRAINT Book_PK PRIMARY KEY (GroupName, BookTitle),
 CONSTRAINT Book_FK_1 FOREIGN KEY (GroupName) REFERENCES WritingGroup (GroupName),
-CONSTRAINT Book_FK_2 FOREIGN KEY (PublisherName) REFERENCES Publisher (PublisherName),
-CONSTRAINT Book_CK_1 UNIQUE (BookTitle),
-CONSTRAINT Book_CK_2 UNIQUE (PublisherName)
+CONSTRAINT Book_FK_2 FOREIGN KEY (PublisherName) REFERENCES Publisher (PublisherName)
 );
 
 
 insert into WritingGroup(GroupName,HeadWriter,YearFormed,Subject) values 
 	('Team Swag', 'Schoobert Doobert', '1996', 'Science Fiction'),
-	('Team Rocket', 'Robert Ugly', '2001', 'Romance');
-	('Doge Think', 'Eustice Grand', '1500', 'Self Help')
+	('Team Rocket', 'Robert Ugly', '2001', 'Romance'),
+	('Doge Think', 'Eustice Grand', '1500', 'Self Help');
 
 
 insert into Publisher(PublisherName,PublisherAddress,PublisherPhone,PublisherEmail) values
 	('Cool Guy Publisher', '3392 Fountain Head', '562-973-4072', 'elpresidente@cgpub.com'),
-	('Rockstar Publishing', '5923 Arrow St' ' 714-253-9427', 'headhoncho@rpub.com'),
+	('Rockstar Publishing', '5923 Arrow St', ' 714-253-9427', 'headhoncho@rpub.com'),
 	('Kick Aff Publisher', '1832 Swag Blvd', '626-278-3736', 'iamking@kapub.com'),
 	('QwertySwag Publishings', '9275 Sick Kick Ave', '213-727-8263', 'elchapo@qspub.com');
 
@@ -49,4 +47,4 @@ insert into Book(BookTitle,YearPublished,NumberPages,PublisherName,GroupName) va
 	('The Hypocracy of Fruit', '1999', '50', 'QwertySwag Publishings', 'Doge Think'),
 	('The Irony of Pizza', '2015', '206', 'QwertySwag Publishings', 'Doge Think'),
 	('The Duality of Man', '2014', '207', 'QwertySwag Publishings', 'Doge Think'),
-	('Fifty Shades of Black)', '2013', '295', 'Kick Aff Publisher', 'Team Rocket');
+	('Fifty Shades of Black)', '2013', 
